45da1d1b83c4c4b5b132b462e24d2323c0bd4e9a
Michal <bihone88@gmail.com>
2017-07-07 13:15:47 +0200

    Complete observed pin exercise

--- observed_pin.rb
+++ observed_pin.rb
@@ -34,189 +34,49 @@
 
 # Detective, we count on you!
 
-
-# MY SOLUTION => NOT RESOLVED !!!!
-# def get_pins(observed)
-#   results = []
-#   new_results = []
-#   temp_results = []
-#   comb_hash = {
-#     "1" => ["1", "2", "4"],
-#     "2" => ["1", "2", "3", "5"],
-#     "3" => ["2", "3", "6"],
-#     "4" => ["1", "4", "5", "7"],
-#     "5" => ["2", "4", "5", "6", "8"],
-#     "6" => ["3", "5", "6", "9"],
-#     "7" => ["4", "7", "8"],
-#     "8" => ["5", "7", "8", "9", "0"],
-#     "9" => ["6", "8", "9"],
-#     "0" => ["8", "0"]
-#   }
-#   observed.split("").each do |char|
-#     results << comb_hash[char]
-#   end
-#   p results
-
-#   # max x = 8
-#   # max y = 5
-
-#   results.each_with_index do |one_combo, idx|
-#     one_combo.each_with_index do |digit, i|
-#       unless results[idx + 1].nil?
-#         results[idx + 1].each do |second_combo|
-#           if temp_results.empty?
-#             temp_results[0] = one_combo[i]
-#           elsif temp_results[0].size == results.size
-#             new_results << temp_results
-#             temp_results = []
-#           else
-#             temp_results[0] += second_combo
-#           end
-#         end
-#       end
-#     end
-#   end
-
-#   new_results.flatten
-#   # 5.times do |y|
-#   #   observed.size.times do |x|
-#   #     unless results[x].nil?
-#   #       unless results[x][y].nil?
-#   #         temp_results << results[x][y]
-#   #       else
-#   #         new_results << temp_results.join
-#   #         temp_results = []
-#   #       end
-#   #     end
-#   #   end
-#   # end
-
-
-#   # FIRST CHECK IF RESULT[X] exist
-#   # check if result[0][0]
-#   # check if result[1][0]
-#   # check if result[2][0]
-#   # check if result[3][0]
-#   # check if result[4][0] == NIL => ADD ONLY 4 ELEMENTS
-
-#   # if result[0][0] not nil
-#   #   if result[1][0] not nil
-#   #     if result[2][0] not nil
-#   #       if result[3][0] not nil
-#   #     else
-#   #       new_results << results[0][0] + results[1][0]
-#   #       new_results << results[0][0] + results[1][1]
-#   #       new_results << results[0][0] + results[1][2]
-#   #       new_results << results[0][0] + results[1][3]
-#   #       new_results << results[0][0] + results[1][4]
-#   #       new_results << results[0][1] + results[1][0]
-#   #     end
-#   #   else
-#   #     new_results << results[0][0]
-#   #     new_results << results[0][1]
-#   #     new_results << results[0][2]
-#   #     new_results << results[0][3]
-#   #   end  
-#   # end  
-  
-  
-#   # new_results << results[0][0] + results[1][0] + results[2][0] + results[3][0]
-#   # new_results << results[0][0] + results[1][0] + results[2][0] + results[3][1]
-#   # new_results << results[0][0] + results[1][0] + results[2][0] + results[3][2]
-#   # new_results << results[0][0] + results[1][0] + results[2][0] + results[3][3]
-#   # new_results << results[0][0] + results[1][0] + results[2][1] + results[3][0]
-#   # new_results << results[0][0] + results[1][0] + results[2][1] + results[3][1]
-#   # new_results << results[0][0] + results[1][0] + results[2][1] + results[3][2]
-#   # new_results << results[0][0] + results[1][0] + results[2][1] + results[3][3]
-#   # new_results << results[0][0] + results[1][0] + results[2][2] + results[3][0]
-#   # new_results << results[0][0] + results[1][0] + results[2][2] + results[3][1]
-#   # new_results << results[0][0] + results[1][0] + results[2][2] + results[3][2]
-#   # new_results << results[0][0] + results[1][0] + results[2][2] + results[3][3]
-#   # new_results << results[0][0] + results[1][1] + results[2][0] + results[3][0]
-#   # new_results.uniq
-
-#   # x = 0
-#   # until results[x].nil?
-#   #   y = 0
-#   #   until results[x][y].nil?
-#   #     unless results[x + 1].nil?
-#   #       unless results[x + 1][y + 1].nil?
-          
-#   #       end
-#   #     end
-#   #     y += 1
-#   #   end
-#   #   x += 1
-#   # end
-  
-#   # iterate through elements from the results[0] with elements from results[1]
-#   # add results[0][0] to results[1][0], add results[0][0] to results[1][1] ...
-#   # results.size.times do |x|
-#   #   # print "x = #{x} "
-#   #   results[x].size.times do |y|
-#   #   # print "x = #{x}, y = #{y} ** "
-#   #     results[x].each do |char_x|
-#   #       # print "char_x = #{char_x} ** "
-#   #       unless results[x + 1].nil?
-#   #         results[x + 1].each do |char_y|
-#   #           new_results << (char_x + char_y)
-#   #         end
-#   #       else
-#   #       end
-#   #     end
-#   #   end
-#   # end
-  
-  
-#       # temp_results.each do |item|
-#     #   new_results[x] = item if new_results[x].nil?
-#     #   new_results[x] += item unless new_results[x].nil?
-#     # end
-#     # temp_results = []
-    
-  
-#   # results => array with n-times elements (n - digits)
-#   # 1-digit pin => 1 element, 2-digit pin => 2 elements etc
-#   # results
-
-
-#   # new_results = results.flatten if new_results.empty?
-# end
-
-# BEST SOLUTION
+# MY SOLUTION
 def get_pins(observed)
-  nums = {
-    "0" => ["0", "8"],
+  combos = {
     "1" => ["1", "2", "4"],
     "2" => ["1", "2", "3", "5"],
     "3" => ["2", "3", "6"],
-    "4" => ["1", "4", "7", "5"],
-    "5" => ["2", "5", "8", "4", "6"],
-    "6" => ["3", "6", "9", "5"],
+    "4" => ["1", "4", "5", "7"],
+    "5" => ["2", "4", "5", "6", "8"],
+    "6" => ["3", "5", "6", "9"],
     "7" => ["4", "7", "8"],
-    "8" => ["0", "5", "7", "8", "9"],
-    "9" => ["6", "8", "9"]
+    "8" => ["5", "7", "8", "9", "0"],
+    "9" => ["6", "8", "9"],
+    "0" => ["8", "0"]
   }
-  digits, ret = Array.new, Array.new
-  observed.chars.each { |e| digits.push(nums[e]) }
-  # "18"
-  # ["1", "8"].each
-  # digits.push(nums["1")
-  # digits.push(nums["8")
-  
-  products = digits[0].product(*digits[1..digits.length])
-  products.each { |e| ret.push(e.join) }
-  ret.sort
+  arr_combos = observed.chars.map { |digit| combos[digit] }
+  arr_all_combos = arr_combos[0].product(*arr_combos[1..arr_combos.size])
+  # arr_all_combos = arr_combos[0].product(*arr_combos[1..-1])
+  arr_all_combos.map { |item| item.join }
 end
 
-# p get_pins("8"), get_pins("11"), get_pins("121"), get_pins("3212")
-p get_pins("11")
+# BEST SOLUTIONS
+def get_pins(digits)
+  base_combos = {
+    "1" => %w(1 2 4),
+    "2" => %w(1 2 3 5),
+    "3" => %w(2 3 6),
+    "4" => %w(1 4 5 7),
+    "5" => %w(2 4 5 6 8),
+    "6" => %w(3 5 6 9),
+    "7" => %w(4 7 8),
+    "8" => %w(5 7 8 9 0),
+    "9" => %w(6 8 9),
+    "0" => %w(8 0)
+  }
+  digits.chars.map { |digit| base_combos[digit] }.reduce do |res, cur_item|
+    res.product(cur_item).map(&:join)
+  end
+end
 
-# a = [1, 2, 3, 4, 5, 5, nil, nil]
-# b = [6, 4, 5]
+def get_pins(digits)
+  combos = ["08 ", "124 ", "2135 ", "326 ", "4157 ", "52468 ", "6359 ", "748 ", "85790 ", "968 "]
+  digits = digits.gsub(/(\d)/) { combos[$1.to_i] }.split.map(&:chars).map(&:to_a)
+  digits.first.product(*digits.drop(1)).map(&:join)
+end
 
-# def change(arr)
-# end
-
-# p a[0..a.size-1]
-  
+p get_pins("8"), get_pins("11"), get_pins("369")
