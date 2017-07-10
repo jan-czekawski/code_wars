c609dd14e9a46a8adfb3c374c72092a2cfef0a02
Michal <bihone88@gmail.com>
2017-07-10 21:06:48 +0200

    Add best solution to fluent calculator

--- fluent_calculator.rb
+++ fluent_calculator.rb
@@ -46,5 +46,18 @@ class Calc
   
 end
 
+# BEST SOLUTIONS
+class Calc
+  { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }.each do |m, n|
+    define_method("#{m}") { @proc ? @proc.call(n) : (@number ||= n ; self ) }
+  end
+  
+  { plus: :+, minus: :-, times: :*, divided_by: :/ }.each do |m, o|
+    define_method("#{m}") { @proc ||= lambda { |a| @number.send(o, a) }; self } 
+  end
+end
+
+
+
 p Calc.new.four.plus.five #, 9)
 p Calc.new.five.times.four #, 9)
