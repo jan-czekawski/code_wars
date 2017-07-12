a96badd20d7cc21d52990a8e9f985886d5e65751
Michal <bihone88@gmail.com>
2017-07-12 18:56:02 +0200

    Add best solutions to camel case method exercise

--- camel_case_method.rb
+++ camel_case_method.rb
@@ -11,9 +11,23 @@
 class String
   def camelcase
     self.gsub(/\b[a-z]/) { |let| let.upcase }.gsub(/\s/, '')
+    # self.gsub(/\b[a-z]/, &:upcase).gsub(/\s/, '')
   end
 end
 
+# BEST SOLUTIONS
+class String
+  def camelcase
+    self.split.map(&:capitalize).join
+    # self.split(" ").map { |word| word.capitalize }.join("")
+  end
+end
+
+class String
+  def camelcase
+    self.gsub(/\w+/, &:capitalize).delete(' ')
+  end
+end
 
 p ('test case'.camelcase) # 'TestCase')
 p ('camel case method'.camelcase) # 'CamelCaseMethod')
