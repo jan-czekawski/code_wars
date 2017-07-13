8471d2d4ea6f1bb5c206cc56474180f56c422840
Michal <bihone88@gmail.com>
2017-07-13 18:09:51 +0200

    Add best solutions to greed good exercise

--- greed_good.rb
+++ greed_good.rb
@@ -43,9 +43,52 @@ def score( dice )
       end
     end
   end
+  
   res
 end
 
+
+# BEST SOLUTIONS
+def score( dice )
+  score = 0
+  (1..6).each do |i|
+    counter = dice.select { |n| n == i }
+    score = (i == 1 ? 1000 : i * 100) if counter.size >= 3
+    # score =+ (i == 1 ? 1000 : i * 100) if counter.size >= 3
+    score += (counter.size % 3) * 100 if i == 1
+    score += (counter.size % 3) * 50 if i == 5
+  end
+  score
+end
+
+def score(dice)
+  dice.sort.join.gsub(/(\d)\1\1|(1|5)/).inject(0) do |sum, num|
+    sum + ($1.to_i * 100 + $2.to_i * 10 ) * (num[0] == '1' ? 10 : 1)
+  end
+end
+
+def score( dice )
+  m = {1 => 100, 5 => 50}
+  (1..6).reduce(0) do|res, i|
+    count = dice.count(i)    
+    res + count/3 * i * (i==1 ? 1000 : 100) + count%3*(m[i].to_i)
+  end
+end
+
+SCORE_MAP = {
+  1 => [0, 100, 200, 1000, 1100, 1200, 2000],
+  2 => [0, 0, 0, 200, 200, 200, 400],
+  3 => [0, 0, 0, 300, 300, 300, 600],
+  4 => [0, 0, 0, 400, 400, 400, 800],
+  5 => [0, 50, 100, 500, 550, 600, 1000],
+  6 => [0, 0, 0, 600, 600, 600, 1200]
+}
+
+def score( dice )
+  (1..6).inject(0) do |score, die|
+    score += SCORE_MAP[die][dice.count(die)]
+  end
+end
 
   # it "should value this as worthless" do
     p score( [2, 3, 4, 6, 2] ) #== 0,   "Should be 0 :-(" );
