5047ccd35a6dc4b60ae85d08364aacdc6595fa7c
Michal <bihone88@gmail.com>
2017-07-14 11:17:11 +0200

    Add best solutions to string average exercise

--- string_average.rb
+++ string_average.rb
@@ -6,6 +6,7 @@
 
 # If the string is empty or includes a number greater than 9, return "n/a"
 
+# MY SOLUTION
 NUM = ["zero", "one", "two", "three", "four",
        "five", "six", "seven", "eight", "nine"]
 
@@ -21,6 +22,28 @@ def average_string(s)
   NUM[res / (res > 0 ? s.split.size : 1)]
 end
 
+# BEST SOLUTIONS
+def average_string(s)
+  map = %w(zero one two three four five six seven eight nine)
+  numbers = s.split
+  return 'n/a' unless numbers.any? && numbers.all? { |n| map.include? n }
+  map[numbers.reduce(0) { |a, n| a += map.index(n) } / numbers.size]
+end
+
+def average_string(s)
+  nums = 'zero one two three four five six seven eight nine'.split
+  return 'n/a' if s.empty?
+  s.split.each{|i| return 'n/a' unless nums.member?(i)}
+  arr = s.split.map{|i| nums.index(i)}
+  nums[arr.reduce(:+)/arr.size]
+end
+
+def average_string(s)
+  n = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+  r = s.split(" ").reduce(s.size==0 ? -999 : 0) do 
+    |a,b| a+(n.index(b)||-999)
+  end / [s.split(" ").size,1].max; r>=0 ? n[r] : "n/a"
+end
 
 p average_string("zero nine five two") # "four")
 p average_string("four six two three") # "three")
