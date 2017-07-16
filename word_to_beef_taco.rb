90a95b5b38e34c9d4a80a861148fefa6f9b24942
Michal <bihone88@gmail.com>
2017-07-16 15:17:09 +0200

    Add clever and best solutions and improve my solution to word to beef taco exercise

--- word_to_beef_taco.rb
+++ word_to_beef_taco.rb
@@ -31,8 +31,9 @@
 # Note that no matter what ingredients are passed, our taco will
 # always have a shell.
 
+# MY SOLUTION
 TACO = { "aeiou" => "beef",  "t" => "tomato", "l" => "lettuce",
-         "c" => "cheese", "g" => "guacamole", "s" => "salsa" }
+        "c" => "cheese", "g" => "guacamole", "s" => "salsa" }
 
 def tacofy(word)
   word.downcase.chars.map { |le| le.gsub(/[aeiou]/, 'beef') }.map do |lt|
@@ -44,14 +45,78 @@ def tacofy(word)
   end.compact.unshift("shell").push("shell")
 end
 
-TACO = { "a" => "beef", "e" => "beef", "i" => "beef", "o" => "beef",
-         "u" => "beef", "t" => "tomato", "l" => "lettuce", "c" => "cheese",
+TACO = { "t" => "tomato", "l" => "lettuce", "c" => "cheese",
          "g" => "guacamole", "s" => "salsa" }
+
+def tacofy(word)
+  recipe = Array.new
+  word.downcase.chars.map do |le| 
+    recipe.push(le =~ /[aeiou]/ ? "beef" : TACO[le])
+  end
+  ["shell"] + recipe.compact + ["shell"]
+end
+
+TACO = { "a" => "beef", "e" => "beef", "i" => "beef", "o" => "beef",
+        "u" => "beef", "t" => "tomato", "l" => "lettuce", "c" => "cheese",
+        "g" => "guacamole", "s" => "salsa" }
 
 def tacofy(word)
   word.downcase.chars.map { |lt| TACO[lt] }.compact.unshift("shell").push("shell")
 end
 
+# CLEVER SOLUTIONS
+def tacofy(word)
+  recipe = { 't'=>'tomato', 'l'=>'lettuce', 'c'=>'cheese', 'g'=>'guacamole', 's'=>'salsa' }
+  word = word.downcase.gsub(/[^a-z]/, '')
+  result = word.chars.map do |letter|
+    if letter.match(/[aeiou]/)
+      'beef'
+    elsif recipe.keys.include?(letter)
+      recipe[letter]
+    end
+  end
+  result = result.compact
+  ['shell'] + result + ['shell']
+end
+
+def tacofy(word)
+  
+  taco_recipe = ["shell"]; word_arr = word.split("")
+  
+  recipe_hash = {"t" => "tomato", "l" => "lettuce", "c" => "cheese", "g" => "guacamole", "s" => "salsa"}
+  
+  word_arr.each { |letter| letter.downcase =~ /[aeiou]/ ? taco_recipe << "beef" : taco_recipe << recipe_hash[letter.downcase] }
+  
+  taco_recipe << "shell"
+    
+  return taco_recipe.compact
+  
+end
+
+def tacofy(word)
+  TacoBuilder.new(word).call
+end
+
+class TacoBuilder
+
+  def initialize(word)
+    @word = word
+    @taco = ["shell"]
+  end
+  
+  def call
+    @word.downcase.chars.each do |char|
+      @taco << "beef" if char.match(/[aeiou]/)
+      @taco << "tomato" if char.match(/t/)
+      @taco << "lettuce" if char.match(/l/)
+      @taco << "cheese" if char.match(/c/)
+      @taco << "guacamole" if char.match(/g/)
+      @taco << "salsa" if char.match(/s/)
+    end
+    @taco << "shell"
+  end
+
+end
 
 
 p tacofy("") #,['shell', 'shell'])
