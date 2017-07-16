b73e04f916d4e10ef21972910a4675d361551d2a
Michal <bihone88@gmail.com>
2017-07-16 13:21:23 +0200

    Add clever solutions to unscrambled eggs exercise

--- unscrambled_eggs.rb
+++ unscrambled_eggs.rb
@@ -10,11 +10,25 @@
 # Kata is supposed to be for beginners to practice regular 
 # expressions, so commenting would be appreciated.
 
-
+# MY SOLUTION
 def unscramble_eggs(word)
   word.gsub(/egg/, '')
 end
 
+# CLEVER SOLUTION
+def unscramble_eggs(word)
+  return word.split('egg').join('')
+end
+
+
+CONSONANTS = ('a'..'z').to_a - %w[a e i u o ]
+def unscramble_eggs(word)
+  word.gsub(/([#{CONSONANTS}])egg/i, '\1')
+end
+
+def unscramble_eggs(word)
+  word.gsub(/([^aeiou])egg/i, '\1')
+end
 
 p unscramble_eggs("ceggodegge heggeregge") #, "code here")
 p unscramble_eggs("FeggUNegg KeggATeggA") #,"FUN KATA")
