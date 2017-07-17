5b854fbe7289f45fd0ecb67ee5a61dc7ee75f5e1
Michal <bihone88@gmail.com>
2017-07-17 22:04:19 +0200

    Add my solution to partial word search exercise

---
+++ partial_word_searching.rb
@@ -0,0 +1,33 @@
+# Write a method that will search an array of strings for all strings that 
+# contain another string, ignoring capitalization. Then return an array of the found strings.
+
+# The method takes two parameters, the query string and the array of strings
+# to search, and returns an array.
+
+# If the string isn't contained in any of the strings in the array, the
+# method returns an array containing a single string: "Empty" (or Nothing
+# in Haskell, or "None" in Python)
+
+# Examples
+
+# If the string to search for is "me", and the array to search is 
+# ["home", "milk", "Mercury", "fish"], the method should return ["home", "Mercury"].
+
+
+def findWord(query, array_of_strings)
+  endarray = []
+  # array_of_strings.select { |str| str.match(Regexp.new(query, true)) }
+  endarray = array_of_strings.select { |str| str =~ Regexp.new(query, true) }
+  endarray.empty? ? ["Empty"] : endarray
+end
+
+
+
+a = ["elf", "bog creature", "bee ","milk","FROGS"]
+p findWord("og", a) # == "bog creature", 'Incorrect.')
+p findWord("og", a) # == "FROGS", 'Incorrect.')
+p findWord("MIL", a) # == "milk", 'Incorrect.')
+p findWord(" ", a) # == "bog creature", 'Incorrect.')
+p findWord(" ", a) # == "bee ", 'Incorrect.')
+p findWord("26", a) # == "Empty", 'Incorrect.')
+
