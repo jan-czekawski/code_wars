cea4b152b2dce7975145f3ab8cfafda015ea1970
Michal <bihone88@gmail.com>
2017-07-18 06:45:03 +0200

    Add best solutions to triangle exercise

--- triangle.rb
+++ triangle.rb
@@ -4,10 +4,20 @@
 
 # (In this case, all triangles must have surface greater than 0 to be accepted).
 
+# MY SOLUTION
 def isTriangle(a,b,c)
   [a, b, c].sort.last < [a, b, c].sort.first(2).inject(:+)
 end
 
+# BEST SOLUTION
+def isTriangle(*a)
+  a.sort![0] + a[1] > a[2]
+end
+
+def isTriangle(a,b,c)
+   a, b, c = [a, b, c].sort
+   a + b > c
+end
 
 p isTriangle(1,2,2) #, true)
 p isTriangle(7,2,2) #, false)
