dbe9094a6f121a53c02fdfdd34a6dc2c2b7185f3
Michal <bihone88@gmail.com>
2017-07-19 19:52:46 +0200

    Add my solution to filter lucky number exercise

---
+++ filter_lucky.rb
@@ -0,0 +1,21 @@
+# Write a function filterLucky/filter_lucky() that accepts a list 
+# of integers and filters the list to only include the elements that contain the digit 7.
+
+# For example,
+
+# ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+# [7,70,17]
+# Don't worry about bad input, you will always receive a finite 
+# list of integers.
+
+# MY SOLUTION
+def filter_lucky(lst)
+  # lst.select { |num| num.to_s =~ /7/ }
+  lst.select { |num| "#{num}" =~ /7/ }
+end
+
+
+    p filter_lucky([7]) # [7], "Expected [7]")
+    p filter_lucky([77,8]) # [77], "Expected [77]")
+    p filter_lucky([71]) # [71], "Expected [71]")
+    p filter_lucky([71,9907,69]) # [71,9907], "Expected [71,9907]")
