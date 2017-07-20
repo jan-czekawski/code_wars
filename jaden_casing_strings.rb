7e4f2cf116f7afc810b6542548b96b01b8619f42
Michal <bihone88@gmail.com>
2017-07-20 12:18:57 +0200

    Add best solutions to jaden casing string exercise

--- jaden_casing_strings.rb
+++ jaden_casing_strings.rb
@@ -18,9 +18,42 @@
 class String
   def toJadenCase
     self.split.map(&:capitalize).join(" ")
+    # split.map(&:capitalize).join(" ")
   end
 end
 
+# BEST SOLUTION
+class String
+  def toJadenCase
+  	first_letter = %r{
+    	(?:\A | [^']) # A beginning of line or anything except an apostroph
+      \b            # A word boundary
+      ([a-z])       # Capturing the first letter
+    }x # Enables nice, commented multiline regex
+    self.gsub(first_letter, &:upcase) # Upcases each match
+  end
+end
+
+class String
+  def toJadenCase
+    split.map(&:capitalize).join(' ')
+  end
+end
+
+class String
+  def toJadenCase
+    # self.gsub(Regexp.union(/^./, /\s\w/), &:upcase)
+    gsub(Regexp.union(/^./, /\s\w/), &:upcase)
+  end
+end
+
+class String
+  def toJadenCase
+    gsub(/(\w|')+/, &:capitalize)
+  end
+end
 
 str = "How can mirrors be real if our eyes aren't real";
-p str.toJadenCase() #, "How Can Mirrors Be Real If Our Eyes Aren't Real")
+# str = 'I say "go"'
+# str = "I say \"go\""
+p str.toJadenCase #, "How Can Mirrors Be Real If Our Eyes Aren't Real")
