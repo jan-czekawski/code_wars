c96b4e12b63a768863390ac00288724fa0b32b7a
Michal <bihone88@gmail.com>
2017-07-20 11:33:58 +0200

    Add my solution to mumbling exercise

---
+++ mumbling.rb
@@ -0,0 +1,20 @@
+# This time no story, no theory. The examples below show you how to write function accum:
+
+# Examples:
+
+# accum("abcd")    # "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt")    # "C-Ww-Aaa-Tttt"
+# The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+# MY SOLUTION
+def accum(s)
+  s.chars.map.with_index { |let, ix| (let.downcase * (ix + 1)).capitalize }.join("-")
+end
+
+
+p accum("ZpglnRxqenU") # "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+p accum("NyffsGeyylB") # "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+p accum("MjtkuBovqrU") # "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+p accum("EvidjUnokmM") # "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+p accum("HbideVbxncC") # "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
