37da085ac53b154a6c5339ee904efc56d645b308
Michal <bihone88@gmail.com>
2017-07-22 09:34:02 +0200

    Add best solutions to 3 and 5 exercise

--- 3_and_5.rb
+++ 3_and_5.rb
@@ -6,9 +6,47 @@
 
 # Note: If the number is a multiple of both 3 and 5, only count it once.
 
-
+# MY SOLUTION
 def solution(number)
   (1..number-1).select { |num| num % 3 == 0 || num % 5 == 0}.inject(:+)
+  # (1..number-1).select { |num| num % 3 == 0 || num % 5 == 0}.inject(0, :+)
+  # (1..number-1).select { |num| num % 3 == 0 || num % 5 == 0}.inject(:+) || 0
+end
+
+# BEST SOLUTIONS
+def solution(number)
+  # (1...number).select { |i| i % 3 == 0 || i % 5 == 0 }.inject(:+)
+  (1...number).select { |i| i % 3 == 0 || i % 5 == 0 }.inject(:+) || 0
+end
+
+def solution(number)
+  num3 = (number-1)/3
+  num5 = (number-1)/5
+  num15 = (number-1)/15
+  (3+3*num3)*num3/2+(5+5*num5)*num5/2-(15+15*num15)*num15/2
+end
+
+def solution(n)
+ 1.upto(n-1).to_a.keep_if { |num| (num % 5 == 0) || (num % 3 == 0) }.reduce(:+)
+end
+
+def solution(number)
+  ans = 0
+  (3...number).step(3) {|n| ans += n}
+  (5...number).step(5) {|n| n % 3 == 0 ? next : ans += n}
+  return ans
+end
+
+def solution(number)
+  candidates = ->(k) { (k % 3) * (k % 5) == 0 }
+  (1...number).select(&candidates).reduce(0, &:+)
 end
 
 p solution(10) # 23
+
+# require "benchmark"
+
+# n = 1_000_000
+# Benchmark.bm do |x|
+#   x.report { n.times { solution(10) } }
+# end
