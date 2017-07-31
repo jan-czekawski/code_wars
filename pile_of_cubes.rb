2383654a0dd544957fb76660b958269446e51aec
Michal <bihone88@gmail.com>
2017-07-31 07:27:59 +0200

    Add best solutions to pile of cubes exercise

--- pile_of_cubes.rb
+++ pile_of_cubes.rb
@@ -26,6 +26,26 @@ def find_nb(m)
   sum > m ? -1 : n - 1
 end
 
+# BEST SOLUTIONS
+def find_nb(m)
+  ct = 0
+  while m > 0
+    ct += 1
+    m -= ct**3
+  end
+  m == 0 ? ct : -1
+end
+
+def find_nb(m)
+  check = Math.sqrt(2 * Math.sqrt(m)).floor
+  (check * (check + 1)/2)**2 == m ? check : -1
+end
+
+def find_nb(m)
+  n = ((m * 4)**0.25).to_i
+  (n**2) * ((n + 1)**2) / 4 == m ? n : -1
+end
+
 p find_nb(4183059834009)# 2022)
 p find_nb(24723578342962)# -1)
 p find_nb(135440716410000)# 4824)
