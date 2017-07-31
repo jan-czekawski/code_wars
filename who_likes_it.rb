67e94d5a5536caaf4f6e00e60ab11cb5afeea253
Michal <bihone88@gmail.com>
2017-07-31 07:48:14 +0200

    Add best solution to who likes it exercise

--- who_likes_it.rb
+++ who_likes_it.rb
@@ -26,6 +26,61 @@ def likes(names)
   "#{str} like#{names.size < 2 ? "s" : ""} this"
 end
 
+# BEST SOLUTIONS
+def likes(names)
+  formats = [
+    'no one likes this',
+    '%s likes this',
+    '%s and %s like this',
+    '%s, %s and %s like this'
+  ]
+  if names.length > 3
+    return '%s, %s and %d others like this' % [names[0], names[1], names.length - 2]
+  else
+    return formats[names.length] % names
+  end
+end
+
+def likes(names)
+  case names.length
+  when 0
+    "no one likes this"
+  when 1
+    "%s likes this" % names
+  when 2
+    "%s and %s like this" % names
+  when 3
+    "%s, %s and %s like this" % names
+  else
+    "%s, %s and %d others like this" %
+      [names[0], names[1], names.length - 2]
+  end
+end
+
+def likes(names)
+  case names.size
+  when 0 
+    "no one likes this"
+  when 1 
+    "#{names[0]} likes this"
+  when 2
+    "#{names[0]} and #{names[1]} like this"
+  when 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+  end
+end
+
+def likes(names)
+  first_1, first_2, *rest = names
+  return 'no one likes this' unless first_1
+  return "#{first_1} likes this" unless first_2
+  return "#{first_1} and #{first_2} like this" if rest.empty?
+  return "#{first_1}, #{first_2} and #{rest.first} like this" if rest.size == 1
+  return "#{first_1}, #{first_2} and #{rest.size} others like this"
+end
+
 p likes([])# 'no one likes this')
 p likes(['Peter'])# 'Peter likes this')
 p likes(['Jacob', 'Alex'])# 'Jacob and Alex like this')
