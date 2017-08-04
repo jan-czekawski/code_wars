c1f92d5ee0c4eb30e2eb77175d887327b7db3652
Michal <bihone88@gmail.com>
2017-08-04 21:47:42 +0200

    Add best solutions to which are in exercise

--- 6kyu/which_are_in.rb
+++ 6kyu/which_are_in.rb
@@ -16,13 +16,55 @@
 
 # Beware: r must be without duplicates.
 
-
+# MY SOLUTION
 def in_array(array1, array2)
   array1.select do |word| 
     array2.any? { |long_word| long_word.match(word)  }
   end.sort.uniq
 end
 
+# BEST SOLUTIONS
+def in_array(array1, array2)
+  array1.select{|s| array2.any?{|w| w.include?(s) } }.sort
+end
+
+def in_array(content, check)
+ check = check.join(' ')
+ content.select { |word| check.include? word }
+        .sort
+end
+
+def in_array( needles, haystack)
+  needles.select {|needle| in_haystack?(needle, haystack)}.sort
+end
+
+def in_haystack?( needle, haystack )
+  haystack.any? {|hay| hay.include?(needle)}
+end
+
+def in_array(array1, array2)
+  array1.select { |word| array2.join =~ /#{word}/ }.sort
+end
+
+def in_array(array1, array2)
+  array2.map { |a2| array1.select { |a1| a2.include? a1 } }.flatten.uniq.sort
+end
+
+def in_array(array1, array2)
+  array1.select { |a1| array2.index{ |a2| a2[a1] } }.uniq.sort 
+end
+
+require "set"
+
+def in_array(array1, array2)
+  ret  = SortedSet.new
+  array2.each do |b|
+    (array1-ret.to_a).each do |a|
+      ret << a if b.include? a
+    end
+  end
+  ret.to_a
+end
 
 a1 = ["arp", "live", "strong"]
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
