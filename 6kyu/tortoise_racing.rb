451d27137b6a38ca1057b8aacbabd1f3fee20cc5
Michal <bihone88@gmail.com>
2017-08-05 12:40:39 +0200

    Add best solution to tortoise racing exercise

--- 6kyu/tortoise_racing.rb
+++ 6kyu/tortoise_racing.rb
@@ -25,18 +25,56 @@ def race(v1, v2, g)
   time_in_s = 0
   
   # v1_per_s, v2_per_s = [v1, v2].map(&(1/3600.0).method(:*))
-  v1_per_s, v2_per_s = (v1 / 3600.0), (v2 / 3600.0)
+  v1_per_s, v2_per_s = v1 / 3600r, v2 / 3600r
   
-  until (g + v1_per_s * time_in_s) <= (0 + v2_per_s * time_in_s)
+  until (g + v1_per_s * time_in_s).to_r <= (0 + v2_per_s * time_in_s).to_r
     time_in_s += 1
   end
-  p "g + v1_per_s * time_in_s=#{g + v1_per_s * time_in_s}"
-  p "0 + v2_per_s * time_in_s=#{v2_per_s * time_in_s}"
   h = time_in_s / 3600
   mn = time_in_s % 3600 / 60 
   s = (time_in_s % 3600 % 60).zero? ? 0 : time_in_s % 3600 % 60 
 
   [h, mn, s]
+end
+
+# BEST SOLUTION
+def race(v1, v2, handicap)
+  return nil if v1 >= v2
+  velocity_diff = v2 - v1
+  
+  hr = handicap / velocity_diff
+  rest_hr = handicap % velocity_diff
+  
+  min = rest_hr * 60 / velocity_diff
+  rest_min = rest_hr * 60 % velocity_diff
+  
+  s = rest_min * 60 / velocity_diff
+  
+  [hr, min, s]
+end
+
+def race(v1, v2, lead)
+  return nil if v1 >= v2
+  time_in_seconds = (lead.fdiv(v2 - v1) * 3600).to_i
+  hour = (time_in_seconds / 3600)
+  min  = (time_in_seconds / 60 ) % 60
+  sec  =  time_in_seconds % 60
+  [hour,min,sec]
+end
+
+def race(v1, v2, g)
+   sec = (g * 3600 / (v2 - v1))
+   [sec / 3600,  (sec / 60) % 60, sec % 60] if v2 >= v1
+end
+
+def race(v1, v2, g)
+    return nil if v1 >= v2
+    
+    vd = v2 - v1
+    d = (g / vd.to_f) * 3600
+    t = Time.at(d).utc
+    
+    [t.hour, t.min, t.sec]
 end
 
 p race(720, 850, 70)# [0, 32, 18])
