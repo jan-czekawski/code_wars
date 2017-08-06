b44072bcf28a615921897e4a161de4a8b6435e94
Michal <bihone88@gmail.com>
2017-08-06 19:34:22 +0200

    Add my solution to is prime number exercise

---
+++ 6kyu/is_prime.rb
@@ -0,0 +1,28 @@
+# Is Prime
+
+# Define a function isPrime that takes one integer argument and returns 
+# true or false depending on if the integer is a prime.
+
+# Per Wikipedia, a prime number (or a prime) is a natural number greater 
+# than 1 that has no positive divisors other than 1 and itself.
+
+# Example
+
+# isPrime(5)
+# => true
+# Assumptions
+
+# You can assume you will be given an integer input.
+# You can not assume that the integer will be only positive. You may 
+# be given negative numbers.
+
+# Test if number is prime
+def isPrime(num)
+  return false if num <= 1
+  (2...num).each { |div| return false if num % div == 0 }
+  true
+end
+
+p isPrime(2)# true)
+p isPrime(4)# false)
+p isPrime(11)# true)
