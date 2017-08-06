c0624f25da77422d6e8032269e1d89a4e0e1aed6
Michal <bihone88@gmail.com>
2017-08-06 21:13:39 +0200

    Add best solutions to a10n exercise

--- 6kyu/word_a10n.rb
+++ 6kyu/word_a10n.rb
@@ -35,6 +35,56 @@ class Abbreviator
   end
 end
 
+# BEST SOLUTIONS
+class Abbreviator
+  def self.abbreviate(string)
+    string.gsub(/(\w)(\w{2,})(\w)/i) { "#{$1}#{$2.size}#{$3}" }
+  end
+end
+
+require 'strscan'
+
+class Abbreviator
+  def self.abbreviate(string)
+    scanner = StringScanner.new(string)
+    result = ''
+    loop do
+      word = scanner.scan(/\w+/)
+      break if word.nil?
+      len = word.length
+      result << ((len >= 4) ? "#{word[0]}#{len-2}#{word[-1]}" : word)
+      sep = scanner.scan(/\W+/)
+      break if sep.nil?
+      result << sep
+    end
+    result
+  end
+end
+
+class Abbreviator
+  def self.abbreviate(string)
+    string.gsub(/([a-z]+)/i) {|w| w.length < 4 ? w : w[0] + (w.length - 2).to_s + w[-1]}
+  end
+end
+
+class Abbreviator
+  def self.abbreviate(string) 
+    string.gsub!(/\w{4,}/) do |match|
+       "#{match[0]}#{match.size-2}#{match[-1]}"
+    end
+  end
+end
+
+class Abbreviator
+# module Abbreviator
+  # module_function
+  
+  def self.abbreviate(string)
+    string.gsub(/(?<=[a-z])[a-z]{2,}(?=[a-z])/i, &:length)
+  end
+end
+
 p Abbreviator.abbreviate("banana")# "b4a", Abbreviator.abbreviate("banana") )
+p Abbreviator.abbreviate("ban1ana")# "b4a", Abbreviator.abbreviate("banana") )
 p Abbreviator.abbreviate("double-barrel")# "d4e-b4l", Abbreviator.abbreviate("double-barrel") )
 p Abbreviator.abbreviate("You, and I, should speak.")# "You, and I, s4d s3k.", Abbreviator.abbreviate("You, and I, should speak.") )
