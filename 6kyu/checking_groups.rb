832c03c4a21fdeebfa87f143646c0aa0700fb4c5
Michal <bihone88@gmail.com>
2017-08-08 21:55:09 +0200

    Add the best solutions to checking groups exercise

--- 6kyu/checking_groups.rb
+++ 6kyu/checking_groups.rb
@@ -32,6 +32,63 @@ def group_check(s)
   s.empty?
 end
 
+def group_check(s)
+  stack = []
+  symbols = { '{' => '}', '[' => ']', '(' => ')' }
+  s.each_char do |c|
+    stack << c if symbols.key?(c)
+    return false if symbols.key(c) && symbols.key(c) != stack.pop
+  end
+  stack.empty?
+end
+
+def group_check s
+  loop do
+    return true if s.empty?
+    c = s.gsub /(\{\})|(\[\])|(\(\))/, ''
+    return false if c == s
+    s = c
+  end
+end
+
+def group_check(s)
+  #Think of strings as noncommutative products of group elements
+  #with {, [, and ( all distinct, and }, ], and ) their "inverses"
+  #keep substituting any (), {}, or [] with ""
+  #if the string fails to reduce, it was formatted incorrectly
+  #at most s.length/2 iterations will be necessary
+  (1..(s.length/2)).each do
+    smaller_s = s.sub(/\(\)|\[\]|\{\}/,"");
+    return false if smaller_s.length==s.length
+    s = smaller_s
+  end
+  
+  s == ""
+end
+
+def group_check(s)
+  lambda{|a| a!=s ? group_check(a) : a==""}.(s.gsub("()","").gsub("[]","").gsub("{}",""))
+end
+
+BRACKETS = { ")" => "(", "}" => "{", "]" => "[" }
+
+def group_check(s)
+  s.chars.each_with_object([]) { |symbol, stack|
+    case symbol
+    when *BRACKETS.values
+      stack.push symbol
+    when *BRACKETS.keys
+      return false unless stack.last == BRACKETS[symbol]
+      stack.pop
+    end
+  }.empty?
+end
+
+def group_check(s)
+  while s.gsub!(/(\(\)|\[\]|\{\})/,'') do; end
+  s.empty?
+end
+
 p group_check("{}([])")# true, "That group was matched correctly")
 p group_check("{[{}[]()[]{}{}{}{}{}{}()()()()()()()()]{{{[[[((()))]]]}}}}(())[[]]{{}}[][][][][][][]({[]})")# true, "That group was matched correctly")
 p group_check("")# true, "That group was matched correctly")
