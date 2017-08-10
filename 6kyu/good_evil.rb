f7ed525a8cb538e865860171e4dfab379f862452
Michal <bihone88@gmail.com>
2017-08-10 19:22:24 +0200

    Add my solution to integer recreation exercise

--- 6kyu/good_evil.rb
+++ 6kyu/good_evil.rb
@@ -52,9 +52,9 @@
 # eradicates all trace of Good" if evil wins, or 
 # "Battle Result: No victor on this battle field" if it ends in a tie.
 
-
+# MY SOLUTION
 def goodVsEvil(good, evil)
-  good_power, evil_power = 0, 0
+  # good_power, evil_power = 0, 0
   # good.split.each_with_index do |num, i|
   #   if i < 3
   #     good_power += num.to_i * (i + 1)
@@ -64,29 +64,29 @@ def goodVsEvil(good, evil)
   #     good_power += num.to_i * 2 * i
   #   end
   # end
-  god = good.split.each_with_index.inject do |(sum), (num, i)|
+  god = good.split.each_with_index.inject(0) do |sum, (num, i)|
           if i < 3
-            sum.to_i + num.to_i * (i + 1)
+            sum + num.to_i * (i + 1)
           elsif i < 5
-            sum.to_i + num.to_i * i
+            sum + num.to_i * i
           elsif i == 5
-            sum.to_i + num.to_i * 2 * i
+            sum + num.to_i * 2 * i
           end
         end
   
   
-  evl = evil.split.each_with_index.inject do |(sum), (num, i)|
+  evl = evil.split.each_with_index.inject(0) do |sum, (num, i)|
           case i
           when 0
-            sum.to_i + num.to_i * (i + 1)
+            sum + num.to_i * (i + 1)
           when 1, 2, 3
-            sum.to_i + num.to_i * 2
+            sum + num.to_i * 2
           when 4
-            sum.to_i + num.to_i * (i - 1)
+            sum + num.to_i * (i - 1)
           when 5
-            sum.to_i + num.to_i * i
+            sum + num.to_i * i
           when 6
-            sum.to_i + num.to_i * (i + 4) 
+            sum + num.to_i * (i + 4) 
           end
         end
   
@@ -99,6 +99,61 @@ def goodVsEvil(good, evil)
   end
   
   "Battle Result: #{str}"
+end
+
+# BEST SOLUTIONS
+def total_worth(counts, worth_table)
+  counts.split.each_with_index.reduce(0) { |sum, (c, i)| sum + ( worth_table[i] * c.to_i ) }
+end 
+
+def goodVsEvil(good, evil)
+  case total_worth(good, [1,2,3,3,4,10]) <=> total_worth(evil, [1, 2, 2, 2, 3, 5, 10])
+  when 1 then "Battle Result: Good triumphs over Evil"
+  when -1 then "Battle Result: Evil eradicates all trace of Good"
+  when 0 then "Battle Result: No victor on this battle field"
+  end 
+end
+
+def goodVsEvil(good, evil)
+  goodtot = good.split(' ').zip([1,2,3,3,4,10]).map { |a, b| a.to_i * b }.reduce(:+)
+  eviltot = evil.split(' ').zip([1,2,2,2,3,5,10]).map { |a, b| a.to_i * b }.reduce(:+)
+  if goodtot > eviltot
+    "Battle Result: Good triumphs over Evil" 
+  elsif eviltot > goodtot
+    "Battle Result: Evil eradicates all trace of Good" 
+  else
+    "Battle Result: No victor on this battle field"
+  end
+end
+
+GOOD_WORTH = [1, 2, 3, 3, 4, 10].freeze
+EVIL_WORTH = [1, 2, 2, 2, 3, 5, 10].freeze
+
+def goodVsEvil(good, evil)
+  sum_worth = ->(sum, (n, worth)) { sum + n.to_i * worth }
+  good_total = good.split.zip(GOOD_WORTH).inject(0, &sum_worth)
+  evil_total = evil.split.zip(EVIL_WORTH).inject(0, &sum_worth)
+
+  result = case good_total <=> evil_total
+           when 1 then "Good triumphs over Evil"
+           when -1 then "Evil eradicates all trace of Good"
+           else "No victor on this battle field"
+           end
+
+  "Battle Result: #{result}"
+end
+
+require "matrix"
+def goodVsEvil(good, evil)
+  
+  sum_good=Vector.elements(good.split(" ").map(&:to_i)).inner_product(Vector[1,2,3,3,4,10])
+  sum_evil=Vector.elements(evil.split(" ").map(&:to_i)).inner_product(Vector[1,2,2,2,3,5,10])
+  if sum_good > sum_evil then
+    return "Battle Result: Good triumphs over Evil"
+    elsif sum_good<sum_evil then
+    return "Battle Result: Evil eradicates all trace of Good"
+    else return "Battle Result: No victor on this battle field"
+  end    
 end
 
 p goodVsEvil('1 0 0 0 0 0', '1 0 0 0 0 0 0')#= 'Battle Result: No victor on this battle field', 'Should be a tie' )
