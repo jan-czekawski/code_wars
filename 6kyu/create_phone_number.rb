47d5ee83b4bf0ff845da4dca8e2b2a635a77e994
Michal <bihone88@gmail.com>
2017-08-12 16:42:09 +0200

    Add the best solutions to create phone number exercise

--- 6kyu/create_phone_number.rb
+++ 6kyu/create_phone_number.rb
@@ -10,8 +10,26 @@
 # this challenge. 
 # Don't forget the space after the closing parenthese!
 
+# MY SOLUTION
 def createPhoneNumber(numbers)
   numbers.join.gsub(/(\d{3})(\d{3})(\d{4})/, '(\1) \2-\3')
 end
 
+# BEST SOLUTIONS
+def createPhoneNumber(array)
+  '(%d%d%d) %d%d%d-%d%d%d%d' % array
+end
+
+def createPhoneNumber(digits)
+  area_code, prefix, *extension = digits.each_slice(3).map(&:join)
+  "(#{area_code}) #{prefix}-#{extension.join}"
+end
+
 p createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])#, "(123) 456-7890");
+
+# require "benchmark"
+# n = 100000
+# Benchmark.bm do |x|
+#   x.report { n.times { createPhoneNumber1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) } }
+#   x.report { n.times { createPhoneNumber2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) } }
+# end
