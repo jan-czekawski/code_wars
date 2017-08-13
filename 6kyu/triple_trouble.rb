4e72d84d1aa962d1eb255a43e3e7626e4c9d05f4
Michal <bihone88@gmail.com>
2017-08-13 15:08:06 +0200

    Add best solutions to triple trouble exercise

--- 6kyu/triple_trouble.rb
+++ 6kyu/triple_trouble.rb
@@ -12,6 +12,26 @@ def triple_double(num1,num2)
   numbers.flatten.empty? ? 0 : 1
 end
 
+def triple_double(num1, num2)
+  num1.to_s.scan(/(.)\1\1/).any? { |n| /#{n}{2}/ === num2.to_s } ? 1 : 0
+  # num1.to_s.scan(/(.)\1\1/).any? { |n| /#{n}{2}/ =~ num2.to_s } ? 1 : 0
+end
+
+def triple_double(num1,num2)
+  num1 = num1.to_s
+  num2 = num2.to_s
+  10.times do |i|
+    if num1.include?(i.to_s*3) && num2.include?(i.to_s*2)
+      return 1
+    end
+  end
+  return 0
+end
+
+def triple_double(num1,num2)
+  "#{num1}-#{num2}".scan(/(\d)\1\1\d*-\d*\1\1/).count
+end
+
 p triple_double(451999277, 41177722899)# 1)
 p triple_double(1222345, 12345)# 0)
 p triple_double(12345, 12345)# 0)
