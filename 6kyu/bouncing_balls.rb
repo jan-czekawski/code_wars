5e5706596d126eefeae17151f2c4bbad4e887ec9
Michal <bihone88@gmail.com>
2017-08-14 13:52:39 +0200

    Add best solutions to bouncing balls exercise

--- 6kyu/bouncing_balls.rb
+++ 6kyu/bouncing_balls.rb
@@ -30,6 +30,7 @@
 
 # h = 3, bounce = 1, window = 1.5, result is -1
 
+# MY SOLUTION
 def bouncingBall(h, bounce, window)
   return -1 if bounce >= 1 || bounce < 0 || window >= h
   count = 0
@@ -40,5 +41,28 @@ def bouncingBall(h, bounce, window)
   count - 1
 end
 
-p bouncingBall(3, 0.66, 1.5)#, 3)
-p bouncingBall(30, 0.66, 1.5)#, 15)
+# BEST SOLUTION
+def bouncingBall(h, bounce, window)
+  bounce > 0 && bounce < 1 && window < h ? 1 + 2*Math.log(window.to_f/h,bounce).floor : -1
+end
+
+def bouncingBall(start, decay, threshold)
+  return -1 if decay >= 1 || decay < 0 || start <= threshold
+  return 1 + (Math.log(threshold.to_f / start) / Math.log(decay)).floor * 2
+end
+
+def bouncingBall(h, bounce, window)
+  bounce < 0 || bounce >= 1 || h <= window || window < 0 ? -1 : bouncingBall4(h * bounce, bounce, window) + 2
+end
+
+# p bouncingBall(3, 0.66, 1.5)#, 3)
+# p bouncingBall(30, 0.66, 1.5)#, 15)
+
+# require "benchmark"
+# n = 100_000
+# Benchmark.bm do |x|
+#   x.report { n.times { bouncingBall1(30, 0.99, 1.5)  } }
+#   x.report { n.times { bouncingBall2(30, 0.99, 1.5)  } }
+#   x.report { n.times { bouncingBall3(30, 0.99, 1.5)  } }
+#   x.report { n.times { bouncingBall4(30, 0.99, 1.5)  } }
+# end
