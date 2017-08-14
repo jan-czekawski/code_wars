b41d3f4b12c80aa2c7dcaa869176138057f76de7
Michal <bihone88@gmail.com>
2017-08-14 12:42:40 +0200

    Add my solution to rectangle to square exercise

---
+++ 6kyu/rectangle_to_square.rb
@@ -0,0 +1,72 @@
+# The drawing below gives an idea of how to cut a given "true" 
+# rectangle into squares ("true" rectangle meaning that the two
+# dimensions are different).
+
+# alternative text
+
+# Can you translate this drawing into an algorithm?
+
+# You will be given two dimensions
+
+# a positive integer length (parameter named lng)
+# a positive integer width (parameter named wdth)
+# You will return an array with the size of each of the squares.
+
+#   sqInRect(5, 3) should return [3, 2, 1, 1]
+#   sqInRect(3, 5) should return [3, 2, 1, 1]
+# #Note: lng == wdth as a starting case would be an entirely 
+# different problem and the drawing is planned to be interpreted
+# with lng != wdth. See kata, Square into Squares. Protect trees!.
+
+# When the initial parameters are so that lng == wdth, the solution
+# [lng] would be the most obvious but not in the spirit of this kata
+# so, in that case, return None/nil/null/Nothing. Return {} with C++.
+
+# In that case the returned structure of C will have its sz component
+# equal to 0. (See the "Solution" and "Examples" tabs)
+
+#   sqInRect(5, 5) should return nil
+
+# def sqInRect(lng, wdth)
+#   return nil if lng == wdth
+#   field = lng * wdth
+#   squares = []
+#   while field > 0
+#     root = Math.sqrt(field)
+#     if (1..3).include?(field)
+#       squares << 1
+#       field -= 1
+#     elsif root % 1 == 0
+#       p "sum=" + squares.inject(0) { |sum, num| sum + num**2 }.to_s
+#       p "field= " + field.to_s
+#       squares << root.to_i
+#       field -= root.to_i
+#     else
+#       field -= 1
+#     end
+#   end
+#   squares
+# end
+
+# MY SOLUTION
+def sqInRect(lng, wdth)
+  return nil if lng == wdth
+  squares = []
+  
+  while true
+    if lng > wdth
+      squares << wdth
+      lng -= wdth
+    elsif lng < wdth
+      squares << lng
+      wdth -= lng
+    else
+      squares << lng
+      return squares
+    end
+  end
+end
+
+p sqInRect(5, 5)# nil)
+p sqInRect(5, 3)# [3, 2, 1, 1])
+p sqInRect(20, 14)# [14, 6, 6, 2, 2, 2])
