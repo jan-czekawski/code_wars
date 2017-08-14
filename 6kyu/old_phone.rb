301a76aa6c24012baec24859b5f599d2fd26e3c7
Michal <bihone88@gmail.com>
2017-08-14 14:56:22 +0200

    Add best solution to old phone exercise

--- 6kyu/old_phone.rb
+++ 6kyu/old_phone.rb
@@ -49,7 +49,7 @@
 # who might be testing out different keyboard layouts, and you
 # want to be able to test new ones rapidly.)
 
-
+# MY SOLUTION
 def presses(phrase)
   phone = [ "1", "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", 
             "PQRS7", "TUV8", "WXYZ9", "*", " 0", "#" ]
@@ -61,6 +61,52 @@ def presses(phrase)
   end.inject(&:+)
 end
 
+# BEST SOLUTIONS
+def presses(phrase)
+  groups = ["1", " 0", "ABC2", "DEF3", "GHI4", "JKL5", "MNO6", "TUV8", "PQRS7", "WXYZ9"]
+  phrase.upcase.chars.map do |c|
+    1 + groups.find { |grp| grp.include?(c) }.index(c)
+  end.reduce(:+)
+end
+
+def presses(phrase)
+  buttons = ['1', 'abc2', 'def3', 'ghi4', 'jkl5', 'mno6', 'pqrs7', 'tuv8', 'wxyz9', ' 0']
+  phrase.downcase.each_char.map {|char|
+    buttons.lazy.map{|b| b.index(char)}.find{|i| i } + 1
+  }.reduce(:+)
+end
+
+def get_char_presses(c)
+  buttons = [ '1',     'ABC2', 'DEF3',
+              'GHI4',  'JKL5', 'MNO6',
+              'PQRS7', 'TUV8', 'WXYZ9',
+              '*',     ' 0',   '#' ]
+  buttons.each { |b| if (res = b.index(c)) then return res + 1; end }
+end
+
+def presses(phrase)
+  phrase.chars.collect { |c| get_char_presses c.upcase }.reduce(:+)
+end
+
+def presses(phrase)
+  layout = ['1', 'ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' 0']
+  phrase.upcase.chars.map {|x| layout.select {|y| y.include?(x)}[0].chars.index(x) + 1}.reduce(:+)
+end
+
+def presses(phrase)
+  phrase.upcase.chars.inject(0){|sum, x| sum + index_of(x)}
+end
+
+def index_of x
+  %w(1 ABC2 DEF3 GHI4 JKL5 MNO6 PQRS7 TUV8 WXYZ9 * # \ 0).each do |key|
+    return key.chars.index(x) + 1 if key.chars.index(x)
+  end
+end
+
+def presses(phrase)
+  al,pr = " 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ","1214444454512312312312312312341231234"
+  phrase.upcase.chars.map{|l|pr[al.index(l)||'0']}.reduce(0){|s,x|s+x.to_i}
+end
 
 p presses("LOL")#, 9
 p presses("HOW R U")#, 13
