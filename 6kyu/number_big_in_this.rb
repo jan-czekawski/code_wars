9910b4c28d0fc8d4d580412359fac1234003f3f2
Michal <bihone88@gmail.com>
2017-08-14 13:22:26 +0200

    Add best solution to number big in this exercise

--- 6kyu/number_big_in_this.rb
+++ 6kyu/number_big_in_this.rb
@@ -15,6 +15,7 @@
 # Error checking for text strings or other invalid inputs is not 
 # required, only valid integers will be passed into the function.
 
+# MY SOLUTION
 def narcissistic?(value)
   val_size = value.to_s.size
   value == value.to_s
@@ -22,5 +23,48 @@ def narcissistic?(value)
                 .inject(0) { |sum, num| sum + (num.to_i)**val_size }
 end
 
+# BEST SOLUTIONS
+def narcissistic?( value )
+  v = value.to_s
+  v.chars.collect { |c| c.to_i**(v.length) }.inject(&:+) == value
+end
+
+def narcissistic?( i )
+  length = i.to_s.size
+  i.to_s.chars.map{ |e| e.to_i**length }.inject(:+) == i
+end
+
+def narcissistic?( value )
+  value == value.to_s.chars.map { |x| x.to_i**value.to_s.size }.reduce(:+)
+end
+
+def narcissistic?( value )
+  digits = extract_digits_from(value)  
+  value == expected_value_for(digits)
+end
+
+def extract_digits_from(number)
+  number.to_s.chars.map(&:to_i)
+end
+
+def expected_value_for(digits)
+  power = digits.size
+  digits.inject(0) do |sum, digit|
+    sum += digit ** power
+  end
+end
+
 p narcissistic?(5)# == true, "5 is narcissistic" )
 p narcissistic?(153) # == true, "153 is narcissistic" )
+
+
+# require "benchmark"
+
+# n = 100_000
+# Benchmark.bm do |x|
+#   x.report { n.times { narcissistic1?(123412341234987123498123) } }
+#   x.report { n.times { narcissistic2?(123412341234987123498123) } }
+#   x.report { n.times { narcissistic3?(123412341234987123498123) } }
+#   x.report { n.times { narcissistic4?(123412341234987123498123) } }
+#   x.report { n.times { narcissistic5?(123412341234987123498123) } }
+# end
