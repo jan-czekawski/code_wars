d0c98b429f8da63490ae36e079c08eba83a35c5e
Michal <bihone88@gmail.com>
2017-08-17 16:24:03 +0200

    Add best solutions to numbers expanded exercise

--- 6kyu/numbers_expanded.rb
+++ 6kyu/numbers_expanded.rb
@@ -10,6 +10,7 @@
 
 # If you liked this kata, check out part 2!!
 
+# MY SOLUTION
 def expanded_form(num)
   size = num.to_s.size
   result = []
@@ -24,6 +25,50 @@ def expanded_form(num)
   result.join(" + ")
 end
 
+# BEST SOLUTION
+def expanded_form(num)
+  res = []
+  len = num.abs.to_s.length
+  num.to_s.chars.each_with_index{|d, i| (res << d + '0' * (len - i - 1)) if d != '0'}
+  res.join(' + ')
+end
+
+def expanded_form(num)
+  num.to_s
+     .chars
+     .reverse
+     .map.with_index { |d, idx| d.to_i * 10**idx }
+     .reject(&:zero?)
+     .reverse
+     .join (' + ')
+end
+
+def expanded_form(num)
+  ans = num.to_s.chars.map.with_index {|n, i| n + ("0" * (num.to_s.chars.slice(i..-1).length - 1))}
+  ans.select {|n| n.to_i > 0}.join(" + ")
+end
+
+def expanded_form(num)
+  num_arr = num.to_s.split('')
+  tick = '1'
+  new_arr = []
+  num_arr.reverse.each do |num|
+    unless num == '0'
+      new_arr << (num.to_i * tick.to_i).to_s
+    end
+    tick << '0'
+  end 
+  new_arr.reverse.join(' + ')
+end
+
+def expanded_form(num)
+  arr = num.to_s.chars
+  s = ''
+  arr.each_with_index do |a, index|
+    s << "#{a.to_i * (10 ** (arr.length - index - 1))} + " if a.to_i > 0
+  end
+  s[0,s.length - 3]
+end
 
 p expanded_form(12)# '10 + 2')
 p expanded_form(42)# '40 + 2')
