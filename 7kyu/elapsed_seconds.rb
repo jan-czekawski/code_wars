c32f725d1cad518cce609d67816525c42eefcff1
Michal <bihone88@gmail.com>
2017-10-04 21:38:48 +0200

    Add best solutions to elapsed seconds exercise

--- 7kyu/elapsed_seconds.rb
+++ 7kyu/elapsed_seconds.rb
@@ -7,8 +7,18 @@
 # Time (Ruby) instances.
 # The start time will always be before the end time.
 
+# MY SOLUTION
 def elapsed_seconds(start_time, end_time)
   end_time.to_i - start_time.to_i
+end
+
+# BEST SOLUTIONS
+def elapsed_seconds(start_time, end_time)
+  end_time - start_time
+end
+
+def elapsed_seconds(start_time, end_time)
+  (end_time - start_time).ceil
 end
 
 p elapsed_seconds(Time.now, Time.now + 20) # 20
