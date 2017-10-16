734e5c4d91ad70076b21ec96edab7bb169cfe8cb
Michal <bihone88@gmail.com>
2017-10-16 18:47:53 +0000

    Add solution to reversed string exercise

---
+++ 7kyu/reversed_string.rb
@@ -0,0 +1,10 @@
+# Complete the solution so that it reverses the string value passed into it.
+
+# solution('world') # returns 'dlrow'
+
+# MY SOLUTION
+def solution(str)
+  str.reverse
+end
+
+p solution("home")
