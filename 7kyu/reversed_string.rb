3711b8c5d3fd1728c17a8bdb55118317d5f5d218
Michal <bihone88@gmail.com>
2017-10-16 18:49:00 +0000

    Add other solutions to reversed string exercise

--- 7kyu/reversed_string.rb
+++ 7kyu/reversed_string.rb
@@ -7,4 +7,29 @@ def solution(str)
   str.reverse
 end
 
+def solution(str)
+  str_array = str.split("")
+  i = 0
+  j = str.length - 1
+  (str.length/2).floor.times do
+    str_array[i], str_array[j] = str_array[j], str_array[i]
+    i += 1
+    j -= 1
+  end
+  str_array.join("")
+end
+
+def solution(str)
+  str.split('').inject('') { |s, b| b + s }
+end
+
+def solution(str=nil)
+   str.reverse if !str.nil?
+end
+
+def solution(str)
+  return str if str.empty?
+  str[-1] + solution(str[0...-1])
+end
+
 p solution("home")
