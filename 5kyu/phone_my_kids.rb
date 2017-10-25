86b6df764eea280ec6fcbd9dd82ae79e886f1d17
Michal <bihone88@gmail.com>
2017-10-25 21:43:31 +0000

    Add my and other solutions to phone my kids exercise

---
+++ 5kyu/phone_my_kids.rb
@@ -0,0 +1,12 @@
+# MY SOLUTION
+class Mother
+  @@subclasses = []
+  
+  def self.inherited subclass
+    @@subclasses << subclass
+  end
+  
+  def self.phone_kids
+    @@subclasses.each { |subclass| subclass.phone }
+  end
+end
