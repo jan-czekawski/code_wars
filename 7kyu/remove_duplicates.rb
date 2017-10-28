197660169295bc9850d1abefef8695fa79eaf6c7
Michal <bihone88@gmail.com>
2017-10-28 16:26:45 +0000

    Add best solutions to removed duplicates exercise

--- 7kyu/remove_duplicates.rb
+++ 7kyu/remove_duplicates.rb
@@ -36,6 +36,19 @@ def unique(integers)
   new_integers
 end
 
+# BEST SOLUTIONS
+def unique(integers)
+  integers & integers
+end
+
+def unique(integers)
+  integers.to_set.to_a
+end
+
+def unique(integers)
+  [] | integers
+end
+
 p unique([])# [])
 p unique([5, 2, 1, 3])# [5, 2, 1, 3])
 p unique([1, 5, 2, 0, 2, -3, 1, 10])# [1, 5, 2, 0, -3, 10])
