a02ca4de5618852af5e3f3309186c0b421dda650
bihone <bihone88@gmail.com>
2017-11-03 18:49:17 +0100

    Add other solutions to vampire numbers exercise

--- 7kyu/vampire_numbers.rb
+++ 7kyu/vampire_numbers.rb
@@ -12,6 +12,7 @@
 # Create a function that can receive two 'fangs' and determine if the 
 # product of the two is a valid vampire number.
 
+# MY SOLUTION
 def vampire_test(a,b)
   multiplicands_hash = Hash.new(0)
   product_hash = Hash.new(0)
@@ -29,6 +30,25 @@ def vampire_test(a,b)
   end
 
   multiplicands_hash == product_hash
+end
+
+# OTHER SOLUTIONS
+def vampire_test(a,b)
+  (a.to_s + b.to_s).chars.sort == (a * b).to_s.chars.sort
+end
+
+def vampire_test(a,b)
+  "#{a}#{b}".chars.sort.join == (a*b).to_s.chars.sort.join
+end
+
+def vampire_test(a,b)
+  product = (a * b).to_s.chars.sort
+  fangs = "#{a}#{b}".chars.sort
+  fangs == product
+end
+
+def vampire_test(a,b)
+  ([] << a.to_s.split(//) << b.to_s.split(//)).flatten!.sort == (a * b).to_s.split(//).sort
 end
 
 p vampire_test(21,6)#, true, "Basic: 21 * 6 = 126 should return true")
