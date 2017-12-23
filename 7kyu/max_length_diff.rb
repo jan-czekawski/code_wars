83213a16e30816438bcc582af215301fc429ac5c
Michal <bihone88@gmail.com>
2017-12-23 06:13:01 +0000

    Add max length difference exercise

---
+++ 7kyu/max_length_diff.rb
@@ -0,0 +1,28 @@
+# You are given two arrays a1 and a2 of strings. Each string is composed with
+# letters from a to z. Let x be any string in the first array and y be any string
+# in the second array.
+
+# Find max(abs(length(x) − length(y)))
+
+# If a1 and/or a2 are empty return -1 in each language except in Haskell 
+# (F#) where you will return Nothing (None).
+
+# #Example:
+
+# s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+# s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+# mxdiflg(s1, s2) --> 13
+# Bash note:
+
+# input : 2 strings with substrings separated by ,
+
+# output: number as a string
+
+# MY SOLUTION
+def mxdiflg(a1, a2)
+	# your code
+end
+
+s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+p mxdiflg(s1, s2)#, 13)
