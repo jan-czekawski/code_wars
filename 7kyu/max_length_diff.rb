7f3c25e99127fc957bae436f20d5be9f13091ddd
Michal <bihone88@gmail.com>
2017-12-23 06:20:30 +0000

    Add my solution to max length exercise

--- 7kyu/max_length_diff.rb
+++ 7kyu/max_length_diff.rb
@@ -21,8 +21,27 @@
 # MY SOLUTION
 def mxdiflg(a1, a2)
 	# your code
+  return -1 if a1.empty? || a2.empty?
+	a1_count, a2_count = [], []
+	
+	a1.each do |str| 
+	  a1_count << str.chars.count
+	end
+	
+	a2.each do |str| 
+	  a2_count << str.chars.count
+	end
+	
+	diff_1 = a1_count.max - a2_count.min
+	diff_2 = a2_count.max - a1_count.min
+  
+  diff_1 > diff_2 ? diff_1 : diff_2
 end
 
 s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+p mxdiflg(s1, s2)#, 13)
+
+s1 = []
 s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
 p mxdiflg(s1, s2)#, 13)
