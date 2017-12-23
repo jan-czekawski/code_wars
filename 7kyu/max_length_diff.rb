cbc781ad264c080c6cc3594b952cd03d2969030d
Michal <bihone88@gmail.com>
2017-12-23 06:33:50 +0000

    Add best solutions to max length difference exercise

--- 7kyu/max_length_diff.rb
+++ 7kyu/max_length_diff.rb
@@ -38,6 +38,58 @@ def mxdiflg(a1, a2)
   diff_1 > diff_2 ? diff_1 : diff_2
 end
 
+# BEST SOLUTIONS
+def mxdiflg(a1, a2)
+  # your code
+  if a1.empty? || a2.empty?
+    return -1
+  end
+  a1.product(a2).map {|x,y| (x.length - y.length).abs}.max
+end
+
+def mxdiflg(a1, a2)
+  return -1 if a1.empty? || a2.empty?
+  min1, max1 = a1.map(&:size).minmax
+  min2, max2 = a2.map(&:size).minmax
+  [max1 - min2, max2 - min1].max
+end
+
+def mxdiflg(a1, a2)  
+  a1 = a1.map(&:size)
+  a2 = a2.map(&:size)
+  
+  [a1.max - a2.min, a1.min - a2.max].map(&:abs).max rescue -1
+end
+
+def mxdiflg(a1, a2)
+  a1.each.with_object([-1]) { |elem, ary|
+    a2.each { |sec_elem| 
+      ary << (elem.size - sec_elem.size).abs
+    }
+  }.max
+end
+
+def mxdiflg(a1, a2)
+  mx = -1
+  a1.each { |x|
+    a2.each{ |y|
+      diff = (x.length - y.length).abs
+      if (diff > mx) then
+        mx = diff
+      end
+    }
+  }
+  mx
+end
+
+def mxdiflg(a, b)
+  an,ax = a.map(&:size).minmax
+  bn,bx = b.map(&:size).minmax
+  [bx-an,ax-bn].max
+rescue
+  -1
+end
+
 s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
 s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
 p mxdiflg(s1, s2)#, 13)
