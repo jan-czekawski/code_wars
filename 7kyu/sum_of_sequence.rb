954993a16b6596da02ccd8e728ca8cd5373a77cf
Michal <bihone88@gmail.com>
2017-12-28 18:30:53 +0000

    Add sum of sequence exercise

---
+++ 7kyu/sum_of_sequence.rb
@@ -0,0 +1,32 @@
+# Your task is to make function, which returns the sum of a sequence of integers.
+
+# The sequence is defined by 3 non-negative values: begin, end, step.
+
+# If begin value is greater than the end, function should returns 0
+
+# Examples
+
+# sequenceSum(2,2,2) === 2
+# sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+# sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+# sequenceSum(1,5,3) === 5 // 1 + 4
+# This is the first kata in the series:
+
+# 1) Sum of a sequence (this kata)
+# 2) Sum of a Sequence [Hard-Core Version]
+
+def sequence_sum(begin_number, end_number, step)
+  #your code here
+end
+
+
+p sequence_sum(2, 6, 2)# 12)
+p sequence_sum(1, 5, 1)# 15)
+p sequence_sum(1, 5, 3)# 5)
+p sequence_sum(0, 15, 3)# 45)
+p sequence_sum(16, 15, 3)# 0)
+p sequence_sum(2, 24, 22)# 26)
+p sequence_sum(2, 2, 2)# 2)
+p sequence_sum(2, 2, 1)# 2)
+p sequence_sum(1, 15, 3)# 35)
+p sequence_sum(15, 1, 3)# 0)
